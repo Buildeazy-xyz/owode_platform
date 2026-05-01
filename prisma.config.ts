3b96a1eef281e5cbfb1e1d78ba7ee141cecc467e
owodealajo <your-github-email@example.com>
2026-05-01 22:08:52 +0100

    fix prisma config datasource url

--- prisma.config.ts
+++ prisma.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'prisma/config'
 import { PrismaPg } from '@prisma/adapter-pg'
 
 export default defineConfig({
+  datasource: {
+    url: process.env.DATABASE_URL!
+  },
   migrate: {
     async adapter() {
       const connectionString = process.env.DATABASE_URL!
