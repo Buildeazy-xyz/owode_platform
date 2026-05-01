63d751b9696714c617e95fdce53018c00ad8cf22
owodealajo <your-github-email@example.com>
2026-05-01 18:54:38 +0100

    fix prisma config to use DATABASE_URL env variable

--- prisma.config.ts
+++ prisma.config.ts
@@ -4,13 +4,10 @@ import { defineConfig } from 'prisma/config'
 export default defineConfig({
   earlyAccess: true,
   schema: path.join('prisma', 'schema.prisma'),
-  datasource: {
-    url: 'postgresql://apple@localhost:5432/owodealajo_db?schema=public'
-  },
   migrate: {
     async adapter() {
       const { PrismaPg } = await import('@prisma/adapter-pg')
-      const connectionString = 'postgresql://apple@localhost:5432/owodealajo_db?schema=public'
+      const connectionString = process.env.DATABASE_URL!
       return new PrismaPg({ connectionString })
     }
   }
