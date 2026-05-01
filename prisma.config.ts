c6655dd8072bfa89c12d13798598416e07d92651
owodealajo <your-github-email@example.com>
2026-05-01 22:22:35 +0100

    update prisma config

--- prisma.config.ts
+++ prisma.config.ts
@@ -4,11 +4,5 @@ import { PrismaPg } from '@prisma/adapter-pg'
 export default defineConfig({
   datasource: {
     url: process.env.DATABASE_URL ?? 'postgresql://apple@localhost:5432/owodealajo_db'
-  },
-  migrate: {
-    async adapter() {
-      const connectionString = process.env.DATABASE_URL ?? 'postgresql://apple@localhost:5432/owodealajo_db'
-      return new PrismaPg({ connectionString })
-    }
   }
 })
